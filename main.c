5cd2244e99f6b00d2e0b577d6db709967c8c373c
wxkz <wxkzwin@gmail.com>
2020-02-25 13:38:01 -0300

    Fix 'Segmentation fault (core dumped)' in main.c

--- main.c
+++ main.c
@@ -3,25 +3,29 @@
 #include "core.h"
 
 int main(int argc, char **argv){
-	_teste();
-	if(strcmp(argv[1], "-s") == 0 || strcmp(argv[1], "search") == 0){
-		printf("search");
-	}
-	if(strcmp(argv[1], "-i") == 0 || strcmp(argv[1], "install") == 0){
-		printf("install");
-	}
-	if(strcmp(argv[1], "-r") == 0 || strcmp(argv[1], "remove") == 0){
-		printf("remove");
-	}
-	if(strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "dependencies") == 0){
-		printf("dependencies");
-	}
+//	_teste();
+	if(argv[1] != NULL){
+		// First parameter
+		if(strcmp(argv[1], "-s") == 0 || strcmp(argv[1], "search") == 0){
+			printf("search");
+		}
+		if(strcmp(argv[1], "-i") == 0 || strcmp(argv[1], "install") == 0){
+			printf("install");
+		}
+		if(strcmp(argv[1], "-r") == 0 || strcmp(argv[1], "remove") == 0){
+			printf("remove");
+		}
+		if(strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "dependencies") == 0){
+			printf("dependencies");
+		}
+	
 
-
-	//
-	if(strcmp(argv[2], "-f") == 0 || strcmp(argv[1], "-force") == 0){
-		printf("forced");
+		// Second parameter
+		if(argv[2] != NULL){
+			if(strcmp(argv[2], "-f") == 0 || strcmp(argv[1], "-force") == 0){
+				printf("forced");
+			}
+		}
 	}
-	else{}
 
 }
